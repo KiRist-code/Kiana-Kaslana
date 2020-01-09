605a4278ef2a164ccbd909ae6d3084e7df92681b
minjunSKY <37296174+minjun9886@users.noreply.github.com>
2020-01-09 17:55:53 +0900

    1.0.1
    add command
    - k.help : introduce bot command

--- kianabot.py
+++ kianabot.py
@@ -1,5 +1,4 @@
 import discord
-import os
 
 client = discord.Client()
 
@@ -15,6 +14,26 @@ async def on_message(message):
 
     if message.content.startswith('k.hello'):
         await message.channel.send('내가 왔노라.')
+    if message.content.startswith('K.hello'):
+        await message.channel.send('내가 왔노라.')
 
-access_token = os.environ["BOT_TOKEN"]
-client.run(access_token)
+@client.event
+async def on_message(message):
+    if message.content.startswith('k.help'):
+        embed = discord.Embed(color=808000)
+        embed.add_field(name="Commands", value="These are kianabot command", inline =False)
+        embed.add_field(name="k.help", value="명령어를 확인합니다.", inline =False)
+        embed.add_field(name="k.hello", value="봇에게 인사를 합니다.", inline=False)
+        embed.add_field(name="이 이후의 명령어는 추후에 개발됩니다.",value="last update 2020/1/9", inline=False)
+        embed.set_thumbnail(url=client.user.avatar_url)
+        await message.channel.send(embed=embed)
+    if message.content.startswith('K.help'):
+        embed = discord.Embed(color=808000)
+        embed.add_field(name="Commands", value="These are kianabot command", inline =False)
+        embed.add_field(name="k.help", value="명령어를 확인합니다.(K.help 도 가능)", inline =False)
+        embed.add_field(name="k.hello", value="봇에게 인사를 합니다.(K.hello 도 가능)", inline=False)
+        embed.add_field(name="이 이후의 명령어는 추후에 개발됩니다.",value="last update 2020/1/9", inline=False)
+        embed.set_thumbnail(url=client.user.avatar_url)
+        await message.channel.send(embed=embed)
+
+client.run('NjY0Njg0MTEzOTY1MzUwOTIz.XhbhRA.ubyVh0pgSZop0NdrC_saNC0Wjow')
